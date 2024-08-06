b912c1261f38552d6e25506f13ccf39652fb456d
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-08-06 01:19:44 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -50,7 +50,7 @@ echo "====== Envsetup Done ======="
 # Lunch
 riseup Mi439_4_19 userdebug
 make installclean
-cd kernel/xiaomi/mithorium-4.19/kernel
+cd kernel/xiaomi/lolz-4.19/kernel
 curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
 croot
 rise b
