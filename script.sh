4809dfc823c567aa44c32a74978d47e4372f59d1
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-08-15 16:41:53 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -20,6 +20,23 @@ echo "============="
 echo "Sync success"
 echo "============="
 
+echo "===== Cherry-pick stuff started ====="
+cd packages/apps/Updater
+git fetch rising --unshallow
+git fetch https://github.com/Phantm7/android_packages_apps_Updater fourteen
+git cherry-pick 022c468
+cd ../../..
+echo "===== Cherry-pick Ended ====="
+
+# Remove overrides
+# Define a list of packages to remove
+echo "===== Remove overrides started ====="
+
+OVER_PACKAGES=("GoogleContacts" "GoogleDialer" "PrebuiltBugle" "dialer")
+for PACKAGEU in "${OVER_PACKAGES[@]}"; do
+find vendor/gms -name 'common-vendor.mk' -exec sed -i "/$PACKAGEU/d" {} \;
+done
+echo "===== Remove overrides Success ====="
 
 # Export
 export BUILD_USERNAME=GamerBoy1234•RisingToSuccess
@@ -36,4 +53,7 @@ make installclean
 echo "============="
 
 # Build rom
+cd kernel/xiaomi/mithorium-4.19/kernel
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
+croot
 rise b
