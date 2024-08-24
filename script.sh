66da8d881b09f7673f8025b2e721b905432253ca
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-08-24 03:30:49 -0400

    No need for cherry-pick anymore

--- script.sh
+++ script.sh
@@ -20,14 +20,6 @@ echo "============="
 echo "Sync success"
 echo "============="
 
-# Git cherry-pick
-cd vendor/lineage
-git remote add los https://github.com/LineageOS/android_vendor_lineage
-git fetch los
-git cherry-pick 198966577ace63573e5be49e03a2e59e32997054
-cd ../..
-echo "===== Cherry-picking Success ====="
-
 # Remove overrides
 # Define a list of packages to remove
 echo "===== Remove overrides started ====="
