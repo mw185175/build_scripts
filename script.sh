7cd619a652c075ec6b7ebda651c32ed016efca86
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-08-28 15:23:46 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -20,10 +20,17 @@ echo "============="
 echo "Sync success"
 echo "============="
 
+# Git cherry-pick
+cd vendor/lineage
+git remote add los https://github.com/LineageOS/android_vendor_lineage
+git fetch los
+git cherry-pick 198966577ace63573e5be49e03a2e59e32997054
+cd ../..
+echo "===== Cherry-picking Success ====="
+
 # Private keys
 git clone https://github.com/Gtajisan/vendor_lineage-priv_keys.git vendor/lineage-priv/keys
 echo "===== cp clone done ====="
-
 
 # Export
 export BUILD_USERNAME=FARHAN_UN
