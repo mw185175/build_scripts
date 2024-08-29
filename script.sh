ab5be343e9724ecfa4c332a98c0b03b8d27ca0a9
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-08-29 06:42:26 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -28,9 +28,6 @@ git cherry-pick 198966577ace63573e5be49e03a2e59e32997054
 cd ../..
 echo "===== Cherry-picking Success ====="
 
-# Private keys
-git clone https://github.com/Gtajisan/vendor_lineage-priv_keys.git vendor/lineage-priv/keys
-echo "===== cp clone done ====="
 
 # Export
 export BUILD_USERNAME=FARHAN_UN
