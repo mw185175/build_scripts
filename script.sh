905a87ca52676688a1cb980da95e185eac9a1056
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-09-11 18:19:07 -0400

    Initial commit

--- script.sh
+++ script.sh
@@ -2,44 +2,59 @@
 
 rm -rf .repo/local_manifests/
 
-# repo init rom
-repo init -u https://github.com/sigmadroid-project/manifest.git -b sigma-14.3 --git-lfs --depth=1
+# Initialize git lfs
+git lfs install
+
+# repo init manifest
+repo init -u https://github.com/TenX-OS/manifest.git -b fourteen --git-lfs --depth=1
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Trijal08/local_manifests -b SigmaDroid-14 --depth=1 .repo/local_manifests
-echo "============================"
-echo "Local manifest clone success"
-echo "============================"
+#git clone https://github.com/Trijal08/local_manifests -b SigmaDroid-14 --depth=1 .repo/local_manifests
+#echo "============================"
+#echo "Local manifest clone success"
+#echo "============================"
 
-# build
+# Sync
 /opt/crave/resync.sh
 echo "============="
 echo "Sync success"
 echo "============="
 
-# Use AOSP clang for no errors
-rm -r prebuilts/clang/host/linux-x86
-git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 --depth=1 prebuilts/clang/host/linux-x86
-
 # Private keys
 git clone https://github.com/Trijal08/vendor_lineage-priv_keys.git vendor/lineage-priv/keys
 
 # Export
-export BUILD_USERNAME=GamerBoy1234•BecomingTooSigma
+export BUILD_USERNAME=GamerBoy1234•10XBetter
 export BUILD_HOSTNAME=crave
 echo "======= Export Done ======"
 
 # Set up build environment
-. build/envsetup.sh
+source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
+# Clone trees
+breakfast husky
+rm -rf device/google/shusky
+rm -rf device/google/zuma
+rm -rf device/google/gs101
+git clone --depth=1 https://github.com/Trijal08/android_device_google_shusky.git -b TenX-OS device/google/shusky
+git clone --depth=1 https://github.com/austineyoung2000/android_device_google_zuma.git -b RisingOS-4.x device/google/zuma
+git clone --depth=1 https://github.com/Jrcable1/android_device_google_gs101.git device/google/gs101
+git clone --depth=1 https://github.com/austineyoung2000/packages_apps_PixelParts.git packages/apps/PixelParts
+git clone --depth=1 https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
+git clone --depth=1 https://github.com/austineyoung2000/android_vendor_google_faceunlock.git vendor/google/faceunlock
+git clone --depth=1 https://github.com/ashoss/vendor_bcr.git vendor/bcr
+git lfs clone --depth=1 https://github.com/austineyoung2000/proprietary_vendor_google_husky.git -b RisingOS vendor/google/husky
+echo "====== Cloning device trees done ======"
+
 # Lunch
-lunch sigma_Mi439_4_19-ap2a-userdebug
+breakfast husky
 make installclean
 echo "============="
 
 # Build ROM
-make bacon
+croot
+brunch husky
