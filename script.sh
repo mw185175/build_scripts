155b1659b7e73452c5444586f11edd298bfe3d54
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-09-13 00:15:45 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,26 +2,31 @@
 
 rm -rf .repo/local_manifests/
 
-# Initialize git lfs
-git lfs install
-
-# repo init manifest
+# repo init rom
 repo init -u https://github.com/TenX-OS/manifest.git -b fourteen --git-lfs --depth=1
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-#git clone https://github.com/Trijal08/local_manifests -b SigmaDroid-14 --depth=1 .repo/local_manifests
-#echo "============================"
-#echo "Local manifest clone success"
-#echo "============================"
+git clone https://github.com/Trijal08/local_manifests -b TenX_OS .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
 
 # Sync
 /opt/crave/resync.sh
 echo "============="
 echo "Sync success"
 echo "============="
+
+# Git cherry-pick
+cd vendor/lineage
+git remote add los https://github.com/LineageOS/android_vendor_lineage
+git fetch los
+git cherry-pick 198966577ace63573e5be49e03a2e59e32997054
+cd ../..
+echo "===== Cherry-picking Success ====="
 
 # Private keys
 git clone https://github.com/Trijal08/vendor_lineage-priv_keys.git vendor/lineage-priv/keys
@@ -35,26 +40,7 @@ echo "======= Export Done ======"
 source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
-# Clone trees
-breakfast husky
-rm -rf device/google/shusky
-rm -rf device/google/zuma
-rm -rf device/google/gs101
-git clone --depth=1 https://github.com/Trijal08/android_device_google_shusky.git -b TenX-OS device/google/shusky
-git clone --depth=1 https://github.com/austineyoung2000/android_device_google_zuma.git -b RisingOS-4.x device/google/zuma
-git clone --depth=1 https://github.com/Jrcable1/android_device_google_gs101.git device/google/gs101
-git clone --depth=1 https://github.com/austineyoung2000/packages_apps_PixelParts.git packages/apps/PixelParts
-git clone --depth=1 https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
-git clone --depth=1 https://github.com/austineyoung2000/android_vendor_google_faceunlock.git vendor/google/faceunlock
-git clone --depth=1 https://github.com/ashoss/vendor_bcr.git vendor/bcr
-git lfs clone --depth=1 https://github.com/austineyoung2000/proprietary_vendor_google_husky.git -b RisingOS vendor/google/husky
-echo "====== Cloning device trees done ======"
-
-# Lunch
-breakfast husky
-make installclean
-echo "============="
-
-# Build ROM
+# Lunch 
+breakfast mi439
 croot
-brunch husky
+brunch mi439 userdebug
