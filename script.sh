ff32d5b9364eff92ec941e087eee5de31a29fa19
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-09-28 06:04:17 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,13 +6,13 @@ rm -rf .repo/local_manifests/
 git lfs install
 
 # repo init manifest
-repo init -u https://github.com/Trijal08/tenx_manifest.git -b fourteen --git-lfs --depth=1
+repo init -u https://github.com/Project-PixelStar/manifest -b 14-qpr3 --git-lfs --depth=1
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Trijal08/local_manifests -b TenX_OS-pixel --depth=1 .repo/local_manifests
+git clone https://github.com/Trijal08/local_manifests -b PixelStar_OS-shusky --depth=1 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -27,7 +27,7 @@ echo "============="
 git clone https://github.com/Trijal08/vendor_lineage-priv_keys.git vendor/lineage-priv/keys
 
 # Export
-export BUILD_USERNAME=GamerBoy1234•10XBetter
+export BUILD_USERNAME=GamerBoy1234294•PixelsOfTheStars
 export BUILD_HOSTNAME=crave
 echo "======= Export Done ======"
 
