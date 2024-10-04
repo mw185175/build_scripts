908f3c6bd5f05400bae6225e34bf86d2f1177435
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-10-04 02:37:12 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,13 +6,13 @@ rm -rf .repo/local_manifests/
 git lfs install
 
 # repo init manifest
-repo init -u https://github.com/Project-PixelStar/manifest -b 14-qpr3 --git-lfs --depth=1
+repo init -u https://github.com/TenX-OS/manifest.git -b fourteen --git-lfs --depth=1
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Trijal08/local_manifests -b PixelStar_OS-shusky --depth=1 .repo/local_manifests
+git clone https://github.com/Trijal08/local_manifests -b TenX_OS-shusky --depth=1 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -27,7 +27,7 @@ echo "============="
 git clone https://github.com/Trijal08/vendor_lineage-priv_keys.git vendor/lineage-priv/keys
 
 # Export
-export BUILD_USERNAME=GamerBoy1234294•PixelsOfTheStars
+export BUILD_USERNAME=GamerBoy1234294•10XBetter
 export BUILD_HOSTNAME=crave
 echo "======= Export Done ======"
 
@@ -36,11 +36,10 @@ source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
 # Lunch
-breakfast pixelstar_husky-ap2a-userdebug
-breakfast pixelstar_husky-userdebug
+breakfast husky
 make installclean -j$(nproc --all)
 echo "============="
 
 # Build ROM
 croot
-brunch pixelstar_husky-userdebug -j$(nproc --all)
+brunch husky -j$(nproc --all)
