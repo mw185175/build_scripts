deabd9d0d108c5cd5b723ef1195442fcb897ee32
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-10-18 15:52:31 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,13 +7,13 @@ rm -rf .repo/local_manifests/
 git lfs install
 
 # repo init manifest
-repo init -u https://github.com/Trijal08/orion_manifest -b 14.0 --git-lfs --depth=1
+repo init -u https://github.com/TenX-OS/manifest.git -b fourteen --git-lfs --depth=1
 echo "====================="
 echo "= Repo init success ="
 echo "====================="
 
 # Local manifests
-git clone https://github.com/Trijal08/local_manifests -b Orion_OS-shusky --depth=1 .repo/local_manifests
+git clone https://github.com/Trijal08/local_manifests -b TenX_OS-shusky --depth=1 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -24,29 +24,17 @@ echo "================"
 echo "= Sync success ="
 echo "================"
 
-# Using RisingOS GMS, need mods
-rm -rf vendor/gapps
-git clone https://gitlab.com/RisingTechOSS/android_vendor_gms.git -b fourteen vendor/gapps
-sed -i 's/vendor\/gms/vendor\/gapps/g' vendor/gapps/*.*
-sed -i 's/vendor\/gms/vendor\/gapps/g' vendor/gapps/common/*.*
-sed -i 's/vendor\/gms/vendor\/gapps/g' vendor/gapps/products/*.*
-echo "============================="
-echo "= GMS/GApps setup succeeded ="
-echo "============================="
-
 # Private keys
 git clone https://github.com/Trijal08/vendor_lineage-priv_keys.git vendor/lineage-priv/keys
 
 # Export
-export BUILD_USERNAME="GamerBoy1234294 • OrionStarsInTheSky"
+export BUILD_USERNAME="GamerBoy1234294 • TenXBetter"
 export BUILD_HOSTNAME="crave"
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 echo "======= Export Done ======"
 
 # Set up build environment
 source build/envsetup.sh
-source vendor/gapps/*.sh*
-rm -rf vendor/gms
 echo "====== Envsetup Done ======="
 
 # Lunch
