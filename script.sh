8b1793ef515330df7b32b0f679049d7520e75f48
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-10-21 00:17:04 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,7 +13,7 @@ echo "= Repo init success ="
 echo "====================="
 
 # Local manifests
-git clone https://github.com/Trijal08/local_manifests -b TenX_OS-shusky --depth=1 .repo/local_manifests
+git clone https://github.com/Trijal08/local_manifests -b SuperiorOS-shusky --depth=1 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
