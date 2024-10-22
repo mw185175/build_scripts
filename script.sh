8bde32b0665504e5c568ae4148c84d5232f3ebf6
mw185175 <162651027+mw185175@users.noreply.github.com>
2024-10-22 16:29:48 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,13 +7,14 @@ rm -rf .repo/local_manifests/
 git lfs install
 
 # repo init manifest
-repo init -u https://github.com/OrionOS-prjkt/android -b 14.0 --git-lfs --depth=1
+repo init -u https://github.com/SomethingOS/manifest -b uvite --depth=1 --git-lfs
+
 echo "====================="
 echo "= Repo init success ="
 echo "====================="
 
 # Local manifests
-git clone https://github.com/Trijal08/local_manifests -b Orion_OS-shusky --depth=1 .repo/local_manifests
+git clone https://github.com/mw185175/local_manifests.git -b SomethingOS-shusky --depth=1 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -31,7 +32,7 @@ chmod a+x create-signed-env.sh
 ./create-signed-env.sh
 
 # Export
-export BUILD_USERNAME="GamerBoy1234294 • OrionStarsInTheSky"
+export BUILD_USERNAME="Matthew W • OrionStarsInTheSky"
 export BUILD_HOSTNAME="crave"
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 echo "======= Export Done ======"
