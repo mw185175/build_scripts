000229a33dcb6aa965972634ae18e381b85b2dbd
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-10-21 23:25:34 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,13 +7,13 @@ rm -rf .repo/local_manifests/
 git lfs install
 
 # repo init manifest
-repo init -u https://github.com/SuperiorOS/manifest.git -b fourteen --git-lfs --depth=1
+repo init -u https://github.com/bananadroid/android_manifest.git -b 14 --git-lfs --depth=1
 echo "====================="
 echo "= Repo init success ="
 echo "====================="
 
 # Local manifests
-git clone https://github.com/Trijal08/local_manifests -b SuperiorOS-shusky --depth=1 .repo/local_manifests
+git clone https://github.com/Trijal08/local_manifests -b BananaDroid-shusky --depth=1 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -30,7 +30,7 @@ chmod a+x create-signed-env.sh
 ./create-signed-env.sh
 
 # Export
-export BUILD_USERNAME="GamerBoy1234294 • OrionStarsInTheSky"
+export BUILD_USERNAME="GamerBoy1234294 • BananasInAndroid"
 export BUILD_HOSTNAME="crave"
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 echo "======= Export Done ======"
