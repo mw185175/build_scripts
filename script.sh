2a18792a56cc0e4645e967d888c7f39a02666406
mw185175 <162651027+mw185175@users.noreply.github.com>
2024-10-23 18:13:28 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,13 +7,13 @@ rm -rf .repo/local_manifests/
 git lfs install
 
 # repo init manifest
-repo init -u https://github.com/OrionOS-prjkt/android -b 14.0 --git-lfs --depth=1
+repo init -u https://github.com/SkylineUI/manifest -b aosp-14 --git-lfs
 echo "====================="
 echo "= Repo init success ="
 echo "====================="
 
 # Local manifests
-git clone https://github.com/Trijal08/local_manifests -b Orion_OS-shusky --depth=1 .repo/local_manifests
+git clone https://github.com/mw185175/local_manifests -b SkylineUI-shusky --depth=1 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
