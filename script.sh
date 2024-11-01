c86a4466e965869250cda864279764a64a554f78
mw185175 <162651027+mw185175@users.noreply.github.com>
2024-11-01 15:03:40 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ rm -rf .repo/local_manifests/
 git lfs install
 
 # repo init manifest
-repo init -u https://github.com/SuperiorOS/manifest.git -b fifteen --git-lfs
+--depth=1 repo init -u https://github.com/SuperiorOS/manifest.git -b fifteen --git-lfs
 echo "====================="
 echo "= Repo init success ="
 echo "====================="
