cad017e66b3319d21f45b9ec78c811c193c0cf49
mw185175 <162651027+mw185175@users.noreply.github.com>
2024-11-05 22:36:26 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,13 +7,13 @@ rm -rf .repo/local_manifests/
 git lfs install
 
 # repo init manifest
-repo init -u https://github.com/VoltageOS/manifest.git -b 15 --depth=1 --git-lfs
+repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 15 --depth=1 --git-lfs
 echo "====================="
 echo "= Repo init success ="
 echo "====================="
 
 # Local manifests
-git clone https://github.com/mw185175/local_manifests -b VoltageOS-15-shusky --depth=1 .repo/local_manifests
+git clone https://github.com/mw185175/local_manifests -b Derpfest-A15-shusky-shusky --depth=1 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
